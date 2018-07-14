ec31a6168abfc551a21b3f52e7bef8b96acbd27a
Basavaraj <Basavaraj@Basavaraj>
2018-07-14 22:58:42 +0530

    Second commit

--- ReadExcelData/src/com/readExcel/Testcase.java
+++ ReadExcelData/src/com/readExcel/Testcase.java
@@ -3,7 +3,7 @@ package com.readExcel;
 import java.io.IOException;
 
 public class Testcase extends CommonFunction {
-
+//THis is modified by basavaraj
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
 		String path = "D:\\Keyword_Driven_Framework\\ReadExcelData\\ReadExcelData.xls";
